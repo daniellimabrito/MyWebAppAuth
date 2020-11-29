b49510fc35270699a839f8334b383d298956f1e7
Daniel Brito <daniellimabrito@gmail.com>
2020-11-29 14:01:52 -0800

    Identity basic model created

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyWebAppAuth.Data;
+
+[assembly: HostingStartup(typeof(MyWebAppAuth.Areas.Identity.IdentityHostingStartup))]
+namespace MyWebAppAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
